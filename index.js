81b84baf792efc4de0098d62ca0b6ec4b76951fc
Tark <39446667+Tarku@users.noreply.github.com>
2022-06-30 16:54:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const BIRTH_DATE = Date.parse('24 Sep 2005 15:21:00 GMT');
 
 window.onload = function() {
+  
   let age_span = document.getElementById("age");
   let age = 0;
   
   let current_date = Date.now();
-  let year_difference = BIRTH_DATE.getFullYear() - current_date.getFullYear()
+  let year_difference = BIRTH_DATE.getUTCFullYear() - current_date.getFullYear()
   
-  let month_difference = BIRTH_DATE.getMonth() - current_date.getMonth()
-  let day_difference = BIRTH_DATE.getDate() - current_date.getDate()
+  let month_difference = BIRTH_DATE.getUTCMonth() - current_date.getMonth()
+  let day_difference = BIRTH_DATE.getUTCDate() - current_date.getDate()
   
   if (month_difference < 0) {
     if (day_difference < 0) {
